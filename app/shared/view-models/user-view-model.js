82544ddb5a88ae9773b3b67d85997b8a4cba237c
Viniciuscs01 <vinicius.cs01@gmail.com>
2015-11-04 23:15:00 -0200

    Finish

--- app/shared/view-models/user-view-model.js
+++ app/shared/view-models/user-view-model.js
@@ -1,3 +1,4 @@
+var validator = require("email-validator");
 var config = require("../../shared/config");
 var fetchModule = require("fetch");
 var observableModule = require("data/observable");
@@ -11,6 +12,27 @@ function User(info) {
         password: info.password || ""
     });
 
+    viewModel.login = function() {
+        return fetchModule.fetch(config.apiUrl + "oauth/token", {
+            method: "POST",
+            body: JSON.stringify({
+                username: viewModel.get("email"),
+                password: viewModel.get("password"),
+                grant_type: "password"
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .then(handleErrors)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            config.token = data.Result.access_token;
+        });
+    };
+    
     viewModel.register = function() {
         return fetchModule.fetch(config.apiUrl + "Users", {
             method: "POST",
@@ -23,6 +45,11 @@ function User(info) {
                 "Content-Type": "application/json"
             }
         }).then(handleErrors);
+    };
+    
+    viewModel.isValidEmail = function() {
+        var email = this.get("email");
+        return validator.validate(email);
     };
 
     return viewModel;
---
+++ app/views/login/login.js
@@ -0,0 +1,33 @@
+var dialogsModule = require("ui/dialogs");
+var UserViewModel = require("../../shared/view-models/user-view-model");
+var user = new UserViewModel({
+    email: "tj.vantoll@gmail.com",
+    password: "password"
+});
+
+var frameModule = require("ui/frame");
+var viewModule = require("ui/core/view");
+
+var email;
+exports.loaded = function(args){
+	var page = args.object;
+	page.bindingContext = user;
+}
+
+exports.signIn = function(){
+	user.login()
+        .then(function() {
+            frameModule.topmost().navigate("views/list/list");
+        }).catch(function(error) {
+            console.log(error);
+            dialogsModule.alert({
+                message: "Unfortunately we could not find your account.",
+                okButtonText: "OK"
+            });
+        });
+}
+
+exports.register = function(){
+	var topmost = frameModule.topmost();
+    topmost.navigate("views/register/register");
+}
